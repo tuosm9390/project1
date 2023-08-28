d5a1e7dfef1eb3c36fe8ceae880b0f10040fa0d4
kangaroo19 <1000jjj@naver.com>
2023-08-28 18:59:03 +0900

    1. 왼쪽 사이드바 작업

---
+++ pages/sidebar/MyPage.js
@@ -0,0 +1,20 @@
+//7번 페이지 이후부터 들어가는 사이드바 상단에 들어가는 컴포넌트
+
+import { Avatar } from '@mui/material';
+import style from '../../styles/myPage.module.css'
+
+
+export default function MyPage(){
+    return (
+        <div className={style.boxWrapper}>
+            <div className={style.boxInner}>
+                <div className={style.userIconWrapper}>
+                    <Avatar className={style.userIcon}/>
+                </div>
+                <div className={style.userNameWrapper}>
+                    <div className={style.userName}>마이페이지</div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/sidebar/leftSidebar.js
+++ pages/sidebar/leftSidebar.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import styles from '../../styles/leftSidebar.module.css'
-
+import SideBar from './SideBar'
+import MyPage from './MyPage';
 function leftSidebar() {
     return (
         <sidebar>
             <div className={styles.leftSidebarContainer}>
                 <div className={styles.sidebarWrapper}>
-                    <div className={styles.profile}></div>
+                    <div className={styles.profile}>
+                        <MyPage/>
+                    </div>
                     <div className={styles.contentsBox}>
+        <SideBar/>
                     </div>
                     <div className={styles.advertisement}>
                         <div></div>
