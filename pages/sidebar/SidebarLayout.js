14e5d9080a33ec253749e604f5ff297c1fa70af2
김상찬 <tuosm123@naver.com>
2023-08-29 19:04:38 +0900

    sidebar css modify

--- pages/sidebar/SidebarLayout.js
+++ pages/sidebar/SidebarLayout.js
@@ -4,8 +4,6 @@ import RightSidebar from './rightSidebar'
 import styles from '../../styles/sidebar.module.css'
 
 function SidebarLayout(props) {
-    console.log((props.children.type.name))
-
     return (
         // 사이드 레이아웃
         <section className={styles.sidebarContainer}>
